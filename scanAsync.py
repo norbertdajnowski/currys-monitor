2d48444837502b84b85c0c368cb7524a8d883db3
norbert.dajnowski <norbert.dajnowski@yorksj.ac.uk>
2021-04-02 04:34:36 +0100

    Async Solution. A queue is required to manage discord webhooks (preventing spam)

--- scanAsync.py
+++ scanAsync.py
@@ -1,11 +1,13 @@
-import requests
+import httpx
+import asyncio
 from bs4 import BeautifulSoup
 import logging
 import dotenv
 import datetime
 import json
-import time
 import urllib3
+import random
+from time import sleep 
 from random_user_agent.user_agent import UserAgent
 from random_user_agent.params import SoftwareName, HardwareType
 from fp.fp import FreeProxy
@@ -21,25 +23,8 @@ CONFIG = dotenv.dotenv_values()
 proxyObject = FreeProxy(country_id=[CONFIG['LOCATION']], rand=True)
 
 STOCK = []
-
-
-def scrape_main_site(headers, proxy):
-
-    print('scrape began')
-    items = []
-    url = 'https://www.scan.co.uk/search?q=rtx'
-    s = requests.Session()
-    html = s.get(url=url, headers=headers, proxies=proxy, verify=False, timeout=15)
-    soup = BeautifulSoup(html.text, 'html.parser')
-    products = soup.find_all('li',  {'class': 'product'})
-    for product in products:
-        item = [product.find('span', {'class': 'description'}).text, #title
-                product.find('span', {'class': 'linkNo'}).text,   #Item ID
-                product.find('span',{'class': 'wishlistheart'})['data-price'],  #Price
-                product.find('a')['href']]     #Link
-        if not item[2] == "":
-            items.append(item)
-    return items
+items = []
+stock_q = asyncio.Queue()
 
 
 def discord_webhook(product_item):
@@ -53,7 +38,7 @@ def discord_webhook(product_item):
     if product_item == 'initial':
         data["embeds"] = [
             {
-                "description" : "Sweat Shop Monitor Launched"
+                "title" : "Sweat Shop Monitor Launched"
             }
         ]
     else:
@@ -61,7 +46,6 @@ def discord_webhook(product_item):
             {
               "title": product_item[0],
               "url":  f'https://www.scan.co.uk{product_item[3]}',
-              "color": int(CONFIG['COLOUR']),
               "fields":[
               {
                 "name": "In-Stock",
@@ -83,7 +67,7 @@ def discord_webhook(product_item):
             }
           ]
 
-    result = requests.post(CONFIG['WEBHOOK'], json = data)
+    result = httpx.post(CONFIG['WEBHOOK'], json = data)
 
     try:
         result.raise_for_status()
@@ -95,7 +79,25 @@ def discord_webhook(product_item):
         logging.info("Payload delivered successfully, code {}.".format(result.status_code))
 
 
-def checker(item):
+def remove_duplicates(mylist):
+
+    return [list(t) for t in set(tuple(element) for element in mylist)]
+
+
+def extractor(products):
+
+    global items
+    for product in products:
+        item = [product.find('span', {'class': 'description'}).text, #title
+            product.find('span', {'class': 'linkNo'}).text,   #Item ID
+            product.find('span',{'class': 'wishlistheart'})['data-price'],  #Price
+            product.find('a')['href']]     #Link
+        if not item[2] == "":
+            items.append(item)
+    items = remove_duplicates(items)
+
+
+def checkExist(item):
 
     for product in STOCK:
         if product == item:
@@ -103,43 +105,31 @@ def checker(item):
     return False
 
 
-def remove_duplicates(mylist):
+async def monitorSession(headers, proxy):
 
-    return [list(t) for t in set(tuple(element) for element in mylist)]
+    global STOCK
 
+    print('New monitor session launched')
+    url = 'https://www.scan.co.uk/search?q=nvidia'
 
-def stockProcess(item):
-
-    if not checker(item):
-        STOCK.append(item)
-        discord_webhook(item)
-
-
-
-def monitor():
-
-    print('STARTING MONITOR')
-    logging.info(msg='Successfully started monitor')
-    discord_webhook('initial')
-    start = 1
-    proxy_no = 0
-
-    proxy_list = CONFIG['PROXY'].split('%')
-    proxy = {"http": proxyObject.get()} if proxy_list[0] == "" else {"http": f"http://{proxy_list[proxy_no]}"}
-    headers = {'User-Agent': user_agent_rotator.get_random_user_agent()}
-    keywords = CONFIG['KEYWORDS'].split('%')
     while True:
         try:
-            items = remove_duplicates(scrape_main_site(headers, proxy))
-            for item in items:
-                #check if any items are new
-                stockProcess(item)
-                time.sleep(float(CONFIG['INTERVAL_DELAY'])) 
+            while not stock_q.empty(): 
+                STOCK = await stock_q.get()
+                
+            async with httpx.AsyncClient(proxies=proxy, verify=False) as client:
+                html = await client.get(url=url, headers=headers, timeout=15)
+                soup = BeautifulSoup(html.text, 'html.parser')
+                extractor(soup.find_all('li',  {'class': 'product'})) 
+                print("Async Request completed") 
 
-            #refresh STOCK list
-            STOCK = items
-            time.sleep(float(CONFIG['MONITOR_DELAY']))
-            start = 0
+            for item in items:
+                if not  checkExist(item):
+                    discord_webhook(item)
+                    await asyncio.sleep(random.uniform(0, 0.5)) 
+
+            await stock_q.put(items)
+            await asyncio.sleep(float(CONFIG['MONITOR_DELAY']))
 
         except Exception as e:
             print(f"Exception found '{e}' - Rotating proxy and user-agent")
@@ -152,6 +142,30 @@ def monitor():
                 proxy = {"http": f"http://{proxy_list[proxy_no]}"}
 
 
+def launchMonitor():
+
+    print('STARTING MONITOR')
+    logging.info(msg='Successfully started monitor')
+    discord_webhook('initial')
+    proxy_list = CONFIG['PROXY'].split('%')
+    proxy_no = 0
+    loop = asyncio.get_event_loop()
+
+    try:
+        for x in range(int(CONFIG['ASYNC_SESSIONS'])):
+
+            proxy = {"http": proxyObject.get()} if proxy_list[0] == "" else {"http": f"http://{proxy_list[x]}"}
+            headers = {'User-Agent': user_agent_rotator.get_random_user_agent()}
+            loop.create_task(monitorSession(headers, proxy))
+
+        loop.run_forever()
+
+    except Exception as e:
+        print(f"Exception found '{e}' - problem with async monitors")
+        logging.error(e)
+        loop.close()
+
+
 if __name__ == '__main__':
     urllib3.disable_warnings()
-    monitor()
+    launchMonitor()
